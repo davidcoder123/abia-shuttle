f3357dffac0abe7a3fd9f36cd9ac6f0e786d6246
david <davidakabueze067@gmail.com>
2026-08-10 16:32:15 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import { Routes, Route } from "react-router-dom";
+
+import "./App.css";
+
+function App() {
+  return (
+    <main>
+      <All />
+    </main>
+  );
+}
+
+export default App;
---
+++ src/pages/SignUp.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function SignUp() {
+  return (
+    <div>
+      Sign
+    </div>
+  )
+}
+
+export default SignUp
