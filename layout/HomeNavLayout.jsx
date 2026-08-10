534630c00537f8213d06d739792ba8eaae64f73d
david <davidakabueze067@gmail.com>
2026-08-10 19:27:15 +0100

    added nav component

--- layout/HomeNavLayout.jsx
+++ layout/HomeNavLayout.jsx
@@ -3,9 +3,9 @@ import { Outlet } from "react-router-dom";
 
 function HomeNavLayout() {
   return (
-    <div>
+    <main>
       <Outlet />
-    </div>
+    </main>
   );
 }
 
--- layout/NavLayout.jsx
+++ layout/NavLayout.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
+import NavBar from "../components/NavBar";
 
 function NavLayout() {
   return (
-    <div>
+    <main>
+      <NavBar/>
       <Outlet />
-    </div>
+    </main>
   );
 }
 
