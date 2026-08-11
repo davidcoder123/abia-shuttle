e7e908896a39dc63387b19fde5835d68ae0f1b65
david <davidakabueze067@gmail.com>
2026-08-11 12:34:40 +0100

    changed the title

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,6 @@ import ContactUs from "./pages/LandingPage/ContactUs";
 import Register from "./pages/LandingPage/Register";
 import Login from "./pages/LandingPage/Login";
 import HomeNavLayout from "../layout/HomeNavLayout";
-import CardSection from "./pages/UserPage/CardSection";
 import Home from "./pages/UserPage/Home";
 import BusRoute from "./pages/UserPage/BusRoute";
 import CardSection from "./pages/UserPage/CardSection";
@@ -15,32 +14,32 @@ import BusSchedule from "./pages/UserPage/BusSchedule";
 import TransactionHistory from "./pages/UserPage/TransactionHistory";
 import Notification from "./pages/UserPage/Notification";
 
-  const router = createBrowserRouter([
-    {
-      path: "/",
-      element: <NavLayout />,
-      children: [
-        { index: true, element: <LandingPage /> },
-        { path: "about", element: <AboutUs /> },
-        { path: "contact", element: <ContactUs /> },
-        { path: "signup", element: <Register /> },
-        { path: "login", element: <Login /> },
-      ],
-    },
-    {
-      path: "/",
-      element: <HomeNavLayout />,
-      children: [
-        { index: true, path: "/home", element: <Home /> },
-        { path: "routes", element: <BusRoute /> },
-        { path: "card", element: <CardSection /> },
-        { path: "schedule", element: <BusSchedule /> },
-        { path: "notifications", element: <Notification /> },
-        { path: "history", element: <TransactionHistory /> },
-      ],
-    },
-  ]);
-  function App() {
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <NavLayout />,
+    children: [
+      { index: true, element: <LandingPage /> },
+      { path: "about", element: <AboutUs /> },
+      { path: "contact", element: <ContactUs /> },
+      { path: "signup", element: <Register /> },
+      { path: "login", element: <Login /> },
+    ],
+  },
+  {
+    path: "/",
+    element: <HomeNavLayout />,
+    children: [
+      { index: true, path: "/home", element: <Home /> },
+      { path: "routes", element: <BusRoute /> },
+      { path: "card", element: <CardSection /> },
+      { path: "schedule", element: <BusSchedule /> },
+      { path: "notifications", element: <Notification /> },
+      { path: "history", element: <TransactionHistory /> },
+    ],
+  },
+]);
+function App() {
   return <RouterProvider router={router} />;
 }
 
